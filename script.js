5354319c7fa15855b3064ef2d05b4611f22c4cff
Fernando Alvarez <87094628+arfermarcos@users.noreply.github.com>
2023-04-12 20:47:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputText = document.getElementById("input-text");
+const outputText = document.getElementById("output-text");
+const encryptBtn = document.getElementById("encrypt-btn");
+const decryptBtn = document.getElementById("decrypt-btn");
+const copyBtn = document.getElementById("copy-btn");
+
